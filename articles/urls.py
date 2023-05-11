7eaab8cd86b9e097488b2346a026116bbbd64fbf
Kim Kyungjin <grace8362@naver.com>
2023-05-11 16:05:01 +0900

    병합 테스트

--- articles/urls.py
+++ articles/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.ArticleView.as_view(), name='articles_view'),
     path('<int:article_id>/', views.ArticleDetailView.as_view(),
          name='articles_detail_view'),
+    path('<int:article_id>/like/', views.LikeView.as_view(), name='likes_view'),
+    path('<int:article_id>/bookmark/', views.BookmarkView.as_view(), name='bookmarks _view'),
 ]
